4b653b13512273773221037c7a782c8c382c0af2
Aniruddha <91339174+aniruddha-10@users.noreply.github.com>
2022-12-07 22:03:02 -0700

    Added the fields for storing the passwords
    Added some fields to store username,password and website as given by the user and also added the hashing of the passwords stored by the user

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import os
 import tkinter as tk
 import sqlite3
 import hashlib
+from tkinter import simpledialog
+from functools import partial
 
 # Creating the Database for the Passwords
 with sqlite3.connect("password_manager.db") as db:
@@ -12,8 +14,30 @@ id INTEGER  PRIMARY KEY,
 password TEXT NOT NULL);
 """)
 
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS passwordmanager(
+id INTEGER  PRIMARY KEY,
+website TEXT NOT NULL,
+username TEXT NOT NULL,
+password TEXT NOT NULL);
+""")
+
+cursor.execute("""
+CREATE TABLE IF NOT EXISTS userpasswords(
+id INTEGER  PRIMARY KEY,
+passwords TEXT NOT NULL);
+""")
+
+
+
+# for the popup
+def popupbox(text):
+    answer = simpledialog.askstring("Input String", text)
+    return answer
+
 # Creating the main window
 root = tk.Tk()
+
 
 def hashing(x):
     hash = hashlib.sha256(x)
@@ -129,11 +153,43 @@ def password_manager():
     for widget in root.winfo_children():
         widget.destroy()
     root.title("Password Manager")
-    canvas = tk.Canvas(root, height=500, width=800, bg="#263D42")
-    canvas.pack()
-    frame = tk.Frame(root, bg="white")
-    frame.place(relwidth=0.8, relheight=0.8, relx=0.1, rely=0.1)
-    tk.Label(root, text="WELCOME TO THE PASSWORD VAULT, YOU CAN NOW STORE YOUR PASSWORDS!!",font=("Arial",14,"bold"),bg="white",foreground="RED").place(x=115,y=80)
+
+    def add_entry():
+        text1 = "Website"
+        text2 = "Username"
+        text3 = "Password"
+
+        website = popupbox(text1)
+        username = popupbox(text2)
+        password = popupbox(text3)
+
+        hashed_pass = hashing(password.encode("utf-8"))
+
+        insert_fields ="""INSERT INTO passwordmanager(website,username,password)
+        VALUES(?, ?, ?)"""
+
+        insert_password ="""INSERT INTO userpasswords(passwords)
+        VALUES(?)"""
+
+        cursor.execute(insert_fields,(website,username,password))
+
+        cursor.execute(insert_password,[(hashed_pass)])
+
+        db.commit()
+        password_manager()
+
+    root.geometry("780x340")
+
+    lb = tk.Label(root, text="Welcome to the Password Manager!!",font=("Arial",18,"bold"),foreground="RED")
+    lb.grid(column=1,padx=120)
+    but = tk.Button(root, text="ADD",font=("Arial",13,"bold"),bg="white",foreground="Green",command=add_entry)
+    but.grid(column=1,row=2)
+
+    def remove_entry(input):
+        cursor.execute("DELETE FROM passwordmanager where id = ?", (input,))
+        db.commit()
+        password_manager()
+
 
     def new_user():
         path ="/Users/aniruddhakhan/Desktop/pythonProject/password_manager.db"
@@ -143,8 +199,37 @@ def password_manager():
         except FileNotFoundError:
             print("No such File found")
 
-    tk.Button(root, text="Switch User?", font=("Bahnschrift 20",14,"bold"), bg="white",foreground="black",command=new_user).place(x=590,y=410)
-    tk.Button(root, text="Close",font=("Bahnschrift 20", 14,"bold"), bg="white",foreground="black",borderwidth=2,command=quit).place(x=500,y=410)
+    # tk.Button(root, text="Switch User?", font=("Bahnschrift 20",14,"bold"), bg="white",foreground="black",command=new_user).place(x=590,y=410)
+    # tk.Button(root, text="Close",font=("Bahnschrift 20", 14,"bold"), bg="white",foreground="black",borderwidth=2,command=quit).place(x=500,y=410)
+
+    lbl = tk.Label(root, text = "WEBSITE", font=("Arial",13,"bold"),foreground="white")
+    lbl.grid(row=3,column=0)
+    lbl = tk.Label(root, text="USERNAME", font=("Arial", 13, "bold"), foreground="white")
+    lbl.grid(row=3, column=1)
+    lbl = tk.Label(root, text="PASSWORD", font=("Arial", 13, "bold"),foreground="white")
+    lbl.grid(row=3, column=2)
+    cursor.execute("SELECT * FROM passwordmanager")
+
+    try:
+        if(cursor.fetchall() != None):
+            i = 0
+            while True:
+                cursor.execute("SELECT * FROM passwordmanager")
+                array = cursor.fetchall()
+                lbl1 = tk.Label(root, text=(array[i][1]), font=("Arial", 13,),foreground="white")
+                lbl1.grid(column=0,row=i+4)
+                lbl1 = tk.Label(root, text=(array[i][2]), font=("Arial", 13,), foreground="white")
+                lbl1.grid(column=1, row=i + 4)
+                lbl1 = tk.Label(root, text=(array[i][3]), font=("Arial", 13,), foreground="white")
+                lbl1.grid(column=2, row=i + 4)
+                button = tk.Button(root, text="delete",command=partial(remove_entry,array[i][0]))
+                button.grid(column=3, row=i+4)
+                i = i + 1
+                cursor.execute("SELECT * FROM passwordmanager")
+                if(len(cursor.fetchall())<=1):
+                    break
+    except:
+        print("Keep Calm and Store Passwords")
 
 cursor.execute("SELECT * FROM masterpassword")
 if cursor.fetchall():
